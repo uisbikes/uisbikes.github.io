d4da0489d6d5b9ffd3de70b8655a1f35daf57366
Sagun P <sagun@pop-os.localdomain>
2022-10-14 23:07:34 -0500

    BCP-13 BCP-14 creating the first UI page as drawn in the whiteboard

---
+++ frontend/src/components/bike-list.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import bikeService from "../services/bike-service";
+import Bike from "./bike";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function BikeList(props) {
+  const [bikeList, setBikeList] = React.useState({});
+
+  const getBikeList = (type, available) => {
+    bikeService
+      .get(type, available)
+      .then((response) => {
+        setBikeList(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getBikeList(props.type, props.available);
+  }, [props.type]);
+
+  return (
+    <div className="BikeList">
+      {bikeList.bikes
+        ? bikeList.bikes.map((a, index) => {
+            return <Bike bike={a} key = {a.toString} available = {props.available} index = {index}/>;
+          })
+        : null}
+    </div>
+  );
+}
+
+export default BikeList;
---
+++ frontend/src/components/login.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Login(props) {
+  const initialUserState = {
+    username: "",
+    password: "",
+  };
+
+  const [user, setUser] = useState(initialUserState);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const login = () => {
+    props.login(user);
+  };
+
+  return (
+    <div className="submit-form">
+      <div>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            required
+            value={user.username}
+            onChange={handleInputChange}
+            name="username"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            className="form-control"
+            id="password"
+            required
+            value={user.password}
+            onChange={handleInputChange}
+            name="password"
+          />
+        </div>
+
+        <button onClick={login} className="btn btn-success">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
