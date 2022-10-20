994a78830b7ab899af45b054b82b49d2499b644b
CharlesJ10 <chiderajnr@gmail.com>
2022-10-20 00:17:58 -0500

    Create functionality for adding bikes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import Bike from "./components/bike";
-import BikeList from "./components/bike-list";
 import Login from "./components/login";
+import Layout from "./components/layout";
+import AddBike from "./components/add-bike";
 
 function App() {
   const [user, setUser] = React.useState(null);
-  const [bikeType, setBikeType] = React.useState("Daily");
 
   async function login(user = null) {
     setUser(user);
@@ -19,58 +18,16 @@ function App() {
     setUser(null);
   }
 
-  async function changeBikeType(event) {
-    setBikeType(event.target.value);
-  }
-
   return (
     <div>
-      <nav
-        className="navbar navbar-light rounded-bottom d-flex flex-row justify-content-between"
-        style={{ backgroundColor: "navy" }}
-      >
-        <a href="/" className="navbar-brand text-light">
-          UIS Bikes
-        </a>
-
-        <div className="nav">
-          <li className="nav-item">
-            <a href={"/login"} className="nav-link text-light">
-              Log In
-            </a>
-          </li>
-        </div>
-      </nav>
-
-      <br></br>
-      <div className="d-flex flex-row justify-content-between">
-        <button className="btn btn-secondary lg">+ Add New Bike</button>
-
-        <select
-          className="form-select form-select-lg"
-          style={{ width: "auto" }}
-          onChange={changeBikeType}
-        >
-          <option value="Daily">Daily Bikes</option>
-          <option value="Weekly">Weekly Bikes</option>
-        </select>
-      </div>
-
-      <br></br>
-
-      <div className="card card-body text-light bg-success">
-        Available {bikeType} Bikes
-      </div>
-
-      <BikeList type={bikeType} available={true} />
-
-      <br></br>
-
-      <div className="card card-body text-light bg-secondary">
-        Unavailable {bikeType} Bikes
-      </div>
-
-      <BikeList type={bikeType} available={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/bikes/add" element={<AddBike />} />
+          <Route path="/" element={<Layout />}>
+          <Route path="*" element={<Layout />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/services/bike-service.js
+++ frontend/src/services/bike-service.js
@@ -4,6 +4,18 @@ class BikeDataService {
   get(type, available) {
     return http.get(`/getBikes?type=${type}&available=${available}`);
   }
+ 
+  addBike(data) {
+    return http.post("/addBike", data);
+  }
+
+  updateBike(data) {
+    return http.put("/updateBike", data)
+  }
+  
+  deleteBike(id, bikeId) {
+    return http.delete(`/deleteBike?id=${id}`, {data:{bike_id: bikeId}})
+  }
 }
 
 export default new BikeDataService();
